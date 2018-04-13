616f26c4cd205b871984e593b36ed666ee6d9f48
abajuradam <abajuradam@yahoo.com>
2018-04-13 03:17:18 +0300

    Some links

--- app/src/main/java/publiclyvisible/publiclyvisible/MainActivity.java
+++ app/src/main/java/publiclyvisible/publiclyvisible/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         btMailRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(),RegisterActivity.class);
+                Intent intent = new Intent(view.getContext(),RegisterEmail.class);
                 startActivityForResult(intent,0);
             }
         });
