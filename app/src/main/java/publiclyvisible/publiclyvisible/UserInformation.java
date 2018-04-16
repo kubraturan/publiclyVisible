6ace753ef85a8c73c34031eef49bfc9085dca033
abajuradam <abajuradam@yahoo.com>
2018-04-16 21:45:44 +0300

    User information added

--- app/src/main/java/publiclyvisible/publiclyvisible/UserInformation.java
+++ app/src/main/java/publiclyvisible/publiclyvisible/UserInformation.java
@@ -2,12 +2,36 @@ package publiclyvisible.publiclyvisible;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserInformation extends AppCompatActivity {
-
+    private EditText etName;
+    private EditText etSurname;
+    private EditText etPhoneNumber;
+    private EditText etBorn;
+    private Button btSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etSurname);
+        etPhoneNumber = (EditText) findViewById(R.id.etPhoneNumber);
+        etBorn = (EditText) findViewById(R.id.etBorn);
+        btSave = (Button) findViewById(R.id.btSave);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("");
+
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.setValue(etName.toString());
+            }
+        });
     }
 }
